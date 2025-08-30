7ebebb16499b36753945d51cd987ef74674c11ed
sandeep sharma <sandeeppandit7725@gmail.com>
2025-08-30 18:19:35 +0530

    update contact information in Footer and Contact components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+1-212-456-7890</li>
-            <li>sadikahmetaydin@gmail.com</li>
+            <li>sandeep@gmail.com</li>
           </ul>
         </div>
 
